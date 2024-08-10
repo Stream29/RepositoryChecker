f6c90208799ac0a182f5115810c19d33b33eb758
Stream <1542763342@qq.com>
2024-08-11 02:22:09 +0800

    feat: Utils.kt(真是一场畅快淋漓的FP)

--- src/main/kotlin/indi/stream/repositorychecker/api/Utils.kt
+++ src/main/kotlin/indi/stream/repositorychecker/api/Utils.kt
@@ -2,6 +2,20 @@ package indi.stream.repositorychecker.api
 
 import java.io.File
 
+fun RepositoryChecker.checkSubfolder(checker: FolderChecker) =
+    check {
+        it.listFiles()
+            .orEmpty()
+            .asSequence()
+            .map(checker)
+            .filterNotNull()
+            .flatMap(List<String>::asSequence)
+            .toList()
+            .let(List<String>::orNull)
+    }
+
+private fun <T> List<T>.orNull(): List<T>? = ifEmpty { null }
+
 // The receiver should be a directory, otherwise NullPointerException will be thrown
 // Without this file itself
 val File.subFilesWithoutFolder: Sequence<File>
