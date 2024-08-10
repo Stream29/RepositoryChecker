43050bb4058b92844033e507e46ff8774cfc9cca
Stream <1542763342@qq.com>
2024-08-11 04:31:19 +0800

    feat: 写了一个最基本的扫描检测

--- src/main/kotlin/indi/stream/repositorychecker/api/Utils.kt
+++ src/main/kotlin/indi/stream/repositorychecker/api/Utils.kt
@@ -11,6 +11,16 @@ fun RepositoryChecker.checkSubfolder(checker: FolderChecker) =
             .flatten()
     }
 
+fun findAnyContains(text: String): FolderChecker =
+    { file ->
+        file.subFilesWithoutFolder
+            .filter { it.name.contains(text) }
+            .count().let{
+                if (it == 0) sequenceOf("未找到名称包含\"${text}\"的文件")
+                else emptySequence()
+            }
+    }
+
 // The receiver should be a directory, otherwise NullPointerException will be thrown
 // Without this file itself
 val File.subFilesWithoutFolder: Sequence<File>
